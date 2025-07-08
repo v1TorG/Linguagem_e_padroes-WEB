c84062414d77efcf456e37089af33c988559f0eb
viTor <119415099+v1TorG@users.noreply.github.com>
2025-07-08 10:58:12 -0300

    tema 04 finalizado

---
+++ tema 04/bloco 04/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector('#h1element').innerHTML = "Texto do h1"
+document.querySelector('.h2element').innerHTML = 'Texto do h2'
+document.querySelector('h3').innerHTML = "Texto do h3"
+
+function alerta(){
+    alert('alerta')
+}
+
+function consolepessoal(){
+    console.log('Alerta console')
+}
